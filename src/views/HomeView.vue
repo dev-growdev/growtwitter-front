4a4162ef7dce5884b6c0dc47155980c19e07d6b2
Marcelo Eltz <marcelo@growdev.com.br>
2024-07-15 14:55:18 -0300

    feat: ajuste component folder correct

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
 import MenuApp from '@/components/Menu.vue';
 import FooterApp from '@/components/Footer.vue';
 import TheWelcome from '../components/TheWelcome.vue';
+import type CardTweet from '@/components/CardTweet.vue';
 </script>
 
 <template>
   <MenuApp />
   <main>
     <TheWelcome />
+    <CardTweet />
   </main>
   <FooterApp />
 </template>
