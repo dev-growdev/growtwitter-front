4f43660a6510c4d67190c44ce0e86ef868329bc1
Pablo Garcia <pablorobertodev@gmail.com>
2024-07-15 10:16:51 -0300

    Created dinamics values

--- src/views/CardTweetView.vue
+++ src/views/CardTweetView.vue
@@ -1,26 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const imgAvatar = ref<string>('');
+const nickname = ref<string>('');
+const userName = ref<string>('');
+const posted_since = ref<string>('');
+const content = ref<string>('');
+const coments = ref<string>('');
+const likes = ref<string>('');
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('url');
+    imgAvatar.value = response.data.imgAvatar;
+    nickname.value = response.data.nickname;
+    userName.value = response.data.userName;
+    posted_since.value = response.data.posted_since;
+    content.value = response.data.content;
+    coments.value = response.data.coments;
+    likes.value = response.data.likes;
+  } catch (error) {
+    console.log(error);
+  }
+});
+</script>
 
 <template>
   <div class="container1">
     <div>
-      <img
-        class="img-avatar"
-        src="https://th.bing.com/th/id/OIP.tXKGs73UYjBEZSclUWLSMAHaHa?rs=1&pid=ImgDetMain"
-        alt="logo1"
-      />
+      <img class="img-avatar" :src="imgAvatar" alt="Avatar" />
     </div>
     <div>
       <div class="tweet-identity">
-        <p class="nick-name">NickName</p>
-        <p>@username</p>
-        <p>posted_since</p>
+        <p class="nick-name">{{ nickname }}</p>
+        <p>{{ userName }}</p>
+        <p>{{ posted_since }}</p>
       </div>
       <div>
-        <p>content</p>
+        <p>{{ content }}</p>
       </div>
       <div class="tweet-pop">
-        <p>💬coments</p>
-        <p>🤍likes</p>
+        <p>💬{{ coments }}</p>
+        <p>🤍{{ likes }}</p>
       </div>
     </div>
   </div>
