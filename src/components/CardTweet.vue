e66a1daf6581b5d96bb0bdb09bba82971291f9f8
Mirna A. Araujo <mirnadocris@gmail.com>
2024-07-16 15:12:14 -0300

    add: CardTweet component on ListCard component

--- src/components/CardTweet.vue
+++ src/components/CardTweet.vue
@@ -1,48 +1,31 @@
 <script setup lang="ts">
-import axios from 'axios';
-import { onMounted, ref } from 'vue';
+import { TweetType } from '@/types';
 
-const imgAvatar = ref<string>('');
-const nickname = ref<string>('');
-const userName = ref<string>('');
-const posted_since = ref<string>('');
-const content = ref<string>('');
-const coments = ref<string>('');
-const likes = ref<string>('');
+interface TweetTypeProps {
+  data: TweetType;
+}
 
-onMounted(async () => {
-  try {
-    const response = await axios.get('url');
-    imgAvatar.value = response.data.imgAvatar;
-    nickname.value = response.data.nickname;
-    userName.value = response.data.userName;
-    posted_since.value = response.data.posted_since;
-    content.value = response.data.content;
-    coments.value = response.data.coments;
-    likes.value = response.data.likes;
-  } catch (error) {
-    console.log(error);
-  }
-});
+defineProps<TweetTypeProps>();
 </script>
 
 <template>
   <div class="container1">
     <div>
-      <img class="img-avatar" :src="imgAvatar" alt="Avatar" />
+      <h1></h1>
+      <!-- <img class="img-avatar" :src="data.imgAvatar" alt="Avatar" /> -->
     </div>
     <div>
       <div class="tweet-identity">
-        <p class="nick-name">{{ nickname }}</p>
-        <p>{{ userName }}</p>
-        <p>{{ posted_since }}</p>
+        <!-- <p class="nick-name">{{ data.nickname }}</p> -->
+        <!-- <p>{{ data.userName }}</p> -->
+        <!-- <p>{{ data.posted_since }}</p> -->
       </div>
       <div>
-        <p>{{ content }}</p>
+        <p>{{ data.content }}</p>
       </div>
       <div class="tweet-pop">
-        <p>💬{{ coments }}</p>
-        <p>🤍{{ likes }}</p>
+        <!-- <p>💬{{ data.coments }}</p> -->
+        <!-- <p>🤍{{ data.likes }}</p> -->
       </div>
     </div>
   </div>
---
+++ src/components/ListCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { TweetType } from '@/types';
+import CardTweet from './CardTweet.vue';
+
+interface Props {
+  tweets: TweetType[];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <h1>LIsTAGEM:</h1>
+  <div>
+    <div class="home-content-card" v-for="tweet in tweets" :key="tweet.id">
+      <CardTweet :data="tweet" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home-content-card {
+  width: 100%;
+  height: 200px;
+  margin-bottom: 10px;
+  padding: 2rem;
+  border-bottom: 2px solid #e9e9e9;
+}
+</style>
