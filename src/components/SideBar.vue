f9d79aa58675b7f0cb4881bc3a102c6759195cff
Matheus Luna <srmatheus6162@gmail.com>
2024-07-16 18:01:54 -0300

    feat: adjusting side-bar  responsivitry

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,96 +2,133 @@
 import growtwitterLogo from '@/components/icons/growtwitterLogo.vue';
 import homePageLogo from '@/components/icons/homePageLogo.vue';
 import ProfileLogo from '@/components/icons/profileLogo.vue';
-import HashTag from '@/components/icons/hashTagLogo.vue'
-interface sideType{
-    name:string
-    hashName:string
-    urlImg:string
+import HashTag from '@/components/icons/hashTagLogo.vue';
+interface sideType {
+  name: string;
+  hashName: string;
+  urlImg: string;
 }
 
 interface SidebarProps {
   item: sideType;
 }
 
-defineProps<SidebarProps>()
+defineProps<SidebarProps>();
 </script>
 
-
 <template>
- <div class="sideBar">
-        <div class="upper">
-          <growtwitterLogo/>
-          <ul>
-            <li><homePageLogo/><span class="li-span">Pagina Inicial</span></li>
-            <li><HashTag/><span class="li-span">Explorar</span></li>
-            <li><ProfileLogo/><span class="li-span">Perfil</span></li>
-          </ul>
-          <button class="sideBtn">Twettar</button>
+  <div class="container-nav">
+    <div class="content">
+      <div class="menu">
+        <div><growtwitterLogo /></div>
+        <div class="icon-container">
+          <div class="icon">
+            <div><homePageLogo class="icon-img" /><span>Pagina Inicial</span></div>
+          </div>
+          <div class="icon">
+            <div><HashTag class="icon-img" /><span>Explorar</span></div>
+          </div>
+          <div class="icon">
+            <div><ProfileLogo class="icon-img" /><span>Perfil</span></div>
+          </div>
         </div>
-        <div class="botton">
-            <div class="picAndName">
-                <img class="pic" :src="item.urlImg" alt="">
-                <div class="name">
-                    <span>{{ item.name }} </span>
-                    <span class="username"> {{item.hashName}}</span>
-                </div>
-            </div>
-            <RouterLink style="color: black; font-size: 10px;" to="/">Sair</RouterLink>
-        </div>
+        <div class="sideBtn"><button>Twettar</button></div>
       </div>
+
+      <div class="perfil">
+        <div class="picAndName">
+          <img class="pic" :src="item.urlImg" alt="" />
+          <div class="name">
+            <span>{{ item.name }} </span>
+            <span class="username"> {{ item.hashName }}</span>
+          </div>
+        </div>
+        <RouterLink style="color: black; font-size: 10px" to="/">Sair</RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-li{
+li {
   list-style-type: none;
 }
-ul{
+ul {
   padding: 0;
 }
-.upper{
-  margin-top: 15px;
+.container-nav {
+  margin-top: 10%;
+  padding-left: 20%;
+  width: 100%;
+  height: 100%;
 }
-.sideBtn{
-  width: 120px;
+
+.content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.menu {
+  display: flex;
+  flex-direction: column;
+  gap: 5%;
+}
+.icon-container {
+  display: flex;
+  flex-direction: column;
+}
+.icon {
+  color: black;
+  margin-bottom: 4%;
+}
+.icon-img {
+  width: 8%;
+}
+
+.icon > div {
+  gap: 5%;
+  display: flex;
+  align-items: center;
+}
+.sideBtn {
+  width: 60%;
+}
+.sideBtn > button {
+  width: 100%;
   border: none;
   border-radius: 20px;
-  background-color: #1C9BF0;
+  background-color: #1c9bf0;
   color: #f1f1f1;
   padding: 5px;
   cursor: pointer;
 }
-.sideBtn:hover{
+
+.sideBtn:hover {
   background-color: gray;
 }
-.li-span{
-  margin-left: 5px;
-}
-.pic{
-    width: 50px;
-    border-radius: 100%;
-    margin-right: 5px;
-}
-.sideBar{
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100%;
-    padding: 10PX
-}
-.username{
-    font-size: 0.375rem;
-    color: gray;
-    margin-top: 0;
-}
-.name{
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-}
-.picAndName{
-    display: flex;
-    flex-direction: row;
-    margin-bottom: 10px;
+
+.pic {
+  width: 50px;
+  border-radius: 100%;
+  margin-right: 5px;
 }
 
+.username {
+  font-size: 0.375rem;
+  color: gray;
+  margin-top: 0;
+}
+.name {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.picAndName {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 10px;
+}
 </style>
