3c9804a59985ed2521f60113440ee2a9293991b7
Bruno Juwer <bruno.juwer1997@gmail.com>
2024-07-16 16:19:30 -0300

    feat: add types to validation and preview avatar

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,13 @@ import type { ProductType } from './ProductType';
 import type { CreateAccountType } from './CreateAccountType';
 import type { TweetType } from './TweetType';
 import type { UserType } from './UserType';
+import RegisterAccountValidationType from './RegisterAccountValidationType';
 
-export type { TweetType, NoteType, ProductType, CreateAccountType, UserType };
+export type {
+  TweetType,
+  NoteType,
+  ProductType,
+  CreateAccountType,
+  UserType,
+  RegisterAccountValidationType
+};
