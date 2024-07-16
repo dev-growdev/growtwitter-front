0db1fbb210936bd0098d72e8cdb2aea68c52e48f
Matheus Luna <srmatheus6162@gmail.com>
2024-07-16 14:56:24 -0300

    feat: add attributes to usertype and adjusting index

---
+++ src/types/UserType.ts
@@ -0,0 +1,8 @@
+export interface UserType {
+  name: string;
+  surname: string;
+  username: string;
+  email: string;
+  password: string;
+  avatar_url: string;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,6 @@ import type { NoteType } from './NoteType';
 import type { ProductType } from './ProductType';
 import type { CreateAccountType } from './CreateAccountType';
 import type { TweetType } from './TweetType';
+import type { UserType } from './UserType';
 
-export type { TweetType, NoteType, ProductType, CreateAccountType };
+export type { TweetType, NoteType, ProductType, CreateAccountType, UserType };
