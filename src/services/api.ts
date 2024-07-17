cfc434cff921457786b4f58ddedbe1b325f5cd84
Marcelo Eltz <marcelo@growdev.com.br>
2024-07-17 14:56:42 -0300

    feat: adicionado env url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     Accept: 'application/json'
   }
