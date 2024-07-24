1f448629d5cf6873f038cf9f6344ff377c02e82a
Gustavo Cortezia <gus.cortezia@gmail.com>
2024-07-24 16:07:29 -0300

    vuetify-login-page

--- src/views/loginView.vue
+++ src/views/loginView.vue
@@ -34,39 +34,38 @@ const handleLogin = async () => {
 </script>
 
 <template>
-  <LoadingDefault v-if="loadingVisible" />
   <div class="background">
-    <div class="container">
-      <div class="info-section">
-        <h1>Growtwitter</h1>
-        <h3>Trabalho final do bloco intermediário</h3>
-        <p>
-          O Growtwitter é a plataforma definitiva para todos os apaixonados por redes sociais que
-          buscam uma experiência familiar e poderosa, semelhante ao Twitter, mas com um toque único.
-          Seja parte desta comunidade que valoriza a liberdade de expressão, a conexão com pessoas
-          de todo o mundo e a disseminação de ideias.
-        </p>
-      </div>
-      <div class="form-section">
-        <h2>Entrar no Growtwitter</h2>
-        <div class="form">
-          <label>E-mail:</label>
-          <input class="form-input" v-model="email" type="text" />
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6" class="bg-blue-darken-2 rounded-s-lg pa-8">
+          <v-card-title>Growtwitter</v-card-title>
+              <v-card-subtitle class="text-white" >Trabalho final do bloco intermediário</v-card-subtitle>
+              <v-card-text>
+                O Growtwitter é a plataforma definitiva para todos os apaixonados por redes sociais que
+                buscam uma experiência familiar e poderosa, semelhante ao Twitter, mas com um toque único.
+                Seja parte desta comunidade que valoriza a liberdade de expressão, a conexão com pessoas
+                de todo o mundo e a disseminação de ideias.
+              </v-card-text>
+        </v-col>
+        <v-col cols="12" sm="6" class="bg-white rounded-e-lg pa-8">
 
-          <label>Password:</label>
-          <input class="form-input" v-model="password" type="password" />
+          <v-card-title >Entrar no Growtwitter</v-card-title>
+    
+          <v-text-field label="Email" v-model="email"></v-text-field>
+          <v-text-field type="password" label="Password" v-model="password"></v-text-field>
 
           <div class="keep-connected-container">
             <label for="keep-connected">Permanecer conectado:</label>
             <input type="checkbox" id="keep-connected" v-model="keepConnected" />
           </div>
 
-          <ButtonDefault class="btn" v-on:click="handleLogin">Entrar</ButtonDefault>
-          <p>Nao tem uma conta? <RouterLink to="/register">Clique aqui</RouterLink></p>
-          <span class="error-message" v-if="error">{{ error }}</span>
-        </div>
-      </div>
-    </div>
+          <v-btn block @click="handleLogin" class="bg-blue-darken-2 my-4">Enviar</v-btn>
+          
+          <v-card-text>Não tem uma conta? <RouterLink to="/register">Clique aqui</RouterLink></v-card-text>
+          <v-card-text><span class="error-message" v-if="error">{{ error }}</span></v-card-text>
+        </v-col>
+        </v-row>
+        </v-container>
   </div>
 </template>
 
