d06aee9538819c4ff68ba674dc25acce86be3500
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-07-24 16:46:07 -0300

    background-overlay-component

---
+++ src/components/BackgroundOverlay.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const visible = ref<boolean>(true);
+</script>
+
+<template>
+  <v-overlay :model-value="visible" class="d-flex align-center justify-center" persistent>
+    <slot></slot>
+  </v-overlay>
+</template>
