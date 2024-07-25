c1fcd59b306cb7cbe3b838e20b8487b7f0523b53
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-07-25 14:32:27 -0300

    removing extra ref

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -3,7 +3,6 @@ import SideBar from '@/components/SideBar.vue';
 import ListCard from '@/components/ListCard.vue';
 import { getUser, showPosts } from '@/services/api';
 import type { TweetType } from '@/types/TweetType';
-import { ref } from 'vue';
 import SpinnerComponent from '@/components/SpinnerComponent.vue';
 import { onMounted, ref } from 'vue';
 import LoadingDefault from '@/components/LoadingDefault.vue';
@@ -15,18 +14,15 @@ const loadingVisible = ref<boolean>(false);
 
 const item = ref<UserType[]>([]);
 
-
 async function handleGetUser() {
   const response = await getUser();
   item.value = response.data.data;
-  console.log(item.value)
+  console.log(item.value);
 }
-
 
 onMounted(() => {
   handleGetUser();
 });
-
 
 const tweets = ref<TweetType[]>([]);
 const endpoint = '/postsbyuserauth';
@@ -73,16 +69,16 @@ const items = [
       </div>
       <div class="home-content">
         <span class="home-content-title">
-            <div class="profile-top">
-              <img class="arrow-profile" src="../assets/icone_seta.svg" alt="">
-              <div class="profile-header">
-                <span class="title"> Perfil de {{item.username}} </span>
-                <p class="tweet-count">x tweets</p>
-                <!-- <img class="profile-pic" src="https://pyxis.nymag.com/v1/imgs/d8e/265/8647a0155d65e195130745751c6682e17d-cowboy-bebop-.rsquare.w330.jpg" alt=""> -->
-              <img class="profile-pic" :src="item.avatar_url ?? default_avatar" alt="">
+          <div class="profile-top">
+            <img class="arrow-profile" src="../assets/icone_seta.svg" alt="" />
+            <div class="profile-header">
+              <span class="title"> Perfil de {{ item.username }} </span>
+              <p class="tweet-count">x tweets</p>
+              <!-- <img class="profile-pic" src="https://pyxis.nymag.com/v1/imgs/d8e/265/8647a0155d65e195130745751c6682e17d-cowboy-bebop-.rsquare.w330.jpg" alt=""> -->
+              <img class="profile-pic" :src="item.avatar_url ?? default_avatar" alt="" />
               <div class="name-username">
-                <h3>{{item.name}} {{ item.surname }}</h3>
-                <h6>@{{item.username}}</h6>
+                <h3>{{ item.name }} {{ item.surname }}</h3>
+                <h6>@{{ item.username }}</h6>
               </div>
             </div>
           </div>
@@ -118,16 +114,16 @@ const items = [
 }
 
 .edit-btn {
-font-size: 0.8rem;
-align-self:first baseline;
-margin-bottom: 1rem;
-margin-left: 1rem;
-background-color: #289ef0;
-border: #289ef0 1px solid;
-color: #ffffff;
-height: 1.3rem;
-width: 3.5rem;
-border-radius: 0.5rem;
+  font-size: 0.8rem;
+  align-self: first baseline;
+  margin-bottom: 1rem;
+  margin-left: 1rem;
+  background-color: #289ef0;
+  border: #289ef0 1px solid;
+  color: #ffffff;
+  height: 1.3rem;
+  width: 3.5rem;
+  border-radius: 0.5rem;
 }
 
 .edit-btn:hover {
