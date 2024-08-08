dd77bbe12b7f7ad0649666095aa187e1515633b0
Matheus Luna <srmatheus6162@gmail.com>
2024-08-08 17:00:35 -0300

    feat: adicionando component para application bar

---
+++ src/components/ApplicationBar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import homePageLogo from '@/components/icons/homePageLogo.vue';
+import HashTagLogo from '@/components/icons/hashTagLogo.vue';
+import ProfileLogo from '@/components/icons/profileLogo.vue';
+</script>
+
+<template>
+  <v-layout>
+    <v-bottom-navigation grow class="d-flex d-md-none">
+      <v-btn>
+        <homePageLogo class="img-icon" />
+        <RouterLink to="/" />
+      </v-btn>
+      <v-btn>
+        <RouterLink to="/explore"><HashTagLogo class="img-icon" /></RouterLink>
+      </v-btn>
+      <v-btn>
+        <RouterLink to="/profile"><ProfileLogo class="img-icon" /></RouterLink>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
+</template>
+
+<style scoped>
+.img-icon {
+  max-width: 16vh;
+  max-height: 25vh;
+}
+</style>
