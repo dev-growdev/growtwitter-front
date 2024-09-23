82aa9c92ada16fc1f8b7ba45b1af38cf48be3754
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-23 19:49:21 -0300

    fix-application-bar

--- src/components/ApplicationBar.vue
+++ src/components/ApplicationBar.vue
@@ -5,20 +5,18 @@ import ProfileLogo from '@/components/icons/profileLogo.vue';
 </script>
 
 <template>
-  <v-layout>
-    <v-bottom-navigation grow class="d-flex d-lg-none">
-      <v-btn>
-        <homePageLogo class="img-icon" />
-        <RouterLink to="/" />
-      </v-btn>
-      <v-btn>
-        <RouterLink to="/explore"><HashTagLogo class="img-icon" /></RouterLink>
-      </v-btn>
-      <v-btn>
-        <RouterLink to="/profile"><ProfileLogo class="img-icon" /></RouterLink>
-      </v-btn>
-    </v-bottom-navigation>
-  </v-layout>
+  <v-bottom-navigation grow>
+    <v-btn>
+      <homePageLogo class="img-icon" />
+      <RouterLink to="/" />
+    </v-btn>
+    <v-btn>
+      <RouterLink to="/explore"><HashTagLogo class="img-icon" /></RouterLink>
+    </v-btn>
+    <v-btn>
+      <RouterLink to="/profile"><ProfileLogo class="img-icon" /></RouterLink>
+    </v-btn>
+  </v-bottom-navigation>
 </template>
 
 <style scoped>
