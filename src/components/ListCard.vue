e60d05b066e91168c421de00f933f111cf8e1d37
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-09-23 19:49:40 -0300

    fix-list-card-responsivity

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -11,19 +11,9 @@ defineProps<Props>();
 </script>
 
 <template>
-  <v-row>
-    <v-col cols="12" class="py-0" v-for="tweet in tweets" :key="tweet.id">
+  <div class="pt-4">
+    <div v-for="tweet in tweets" :key="tweet.id">
       <CardTweet :data="tweet" />
-    </v-col>
-  </v-row>
+    </div>
+  </div>
 </template>
-
-<!-- <style scoped>
-.home-content-card {
-  width: auto;
-  height: 100%;
-  border-bottom: 2px solid #e9e9e9;
-  padding: 10px;
-  margin: 5px;
-}
-</style> -->
