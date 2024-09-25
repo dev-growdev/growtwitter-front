4d22cae10d7e8d443039e9176d87d4809ce5a3cf
Matheus Luna <srmatheus6162@gmail.com>
2024-09-25 11:33:06 -0300

    adjusting the code

--- src/utils/isLogged.ts
+++ src/utils/isLogged.ts
@@ -2,7 +2,7 @@ import { useRouter } from 'vue-router';
 
 export async function isLogged() {
   const token = sessionStorage.getItem('token') || localStorage.getIem('token');
-  const user = localStorage.getItem('userData') || sessionStorage.getItem('userData');
+  const user = sessionStorage.getItem('userData') || localStorage.getItem('userData');
   const router = useRouter();
   if (user && token) return router.push('/'); // verifica se tem user e token
   sessionStorage.removeItem('token');
