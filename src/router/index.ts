5d1b30ad49b469ea6425e60bdfa4245e65a8f54c
Bernardo Dartora <bernardintd@gmail.com>
2024-09-25 15:11:54 -0300

    add meta title para cabeçario da pagina

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,29 +12,49 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Home - GrowTwitter'
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: LoginView
+      component: LoginView,
+      meta: {
+        title: 'Login - GrowTwitter'
+      }
     },
     {
       path: '/register',
       name: 'register',
-      component: RegisterView
+      component: RegisterView,
+      meta: {
+        title: 'Register - GrowTwitter'
+      }
     },
     {
       path: '/profile/:id',
       name: 'profile',
-      component: ProfileView
+      component: ProfileView,
+      meta: {
+        title: 'Perfil - GrowTwitter'
+      }
     },
     {
       path: '/explore',
       name: 'explore',
-      component: ExploreView
+      component: ExploreView,
+      meta: {
+        title: 'Explorar - GrowTwitter'
+      }
     }
   ]
+});
+
+router.afterEach((to) => {
+  const nearestTitle = to.meta?.title;
+  if (nearestTitle) document.title = nearestTitle;
 });
 
 router.beforeEach((to) => {
