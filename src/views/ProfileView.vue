bacb2a0837f527fb48e323677b3213828a77f98e
Douglas Santos <foxgoldseller@gmail.com>
2024-09-25 11:40:13 -0300

    fix: ordem do perfil seguidores e seguindo, tipagem de algumas funçoes, não poder seguir você mesmo

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -180,7 +180,7 @@ const handleEdit = async () => {
 const isFollowing = ref<boolean>(false);
 const btnLoading = ref<boolean>(false);
 
-async function getFollowersAndFollowing(id: number) {
+async function getFollowersAndFollowing(id: string) {
   try {
     const response = await getFollowersAndFollowingById(id);
     if (
@@ -199,7 +199,7 @@ async function handleFollow() {
     btnLoading.value = true;
     console.log(anotherUser.value.followers_count);
 
-    const response = await postFollow(route.params.id, item.value.id);
+    const response = await postFollow(route.params.id as string, String(item.value.id));
     console.log(response);
     if (isFollowing.value) {
       anotherUser.value.following_count--;
@@ -214,7 +214,7 @@ async function handleFollow() {
   }
 }
 
-async function getuserbyid(id: number) {
+async function getuserbyid(id: string) {
   try {
     loadingVisible.value = true;
     const response = await getUserbyId(id);
@@ -227,8 +227,8 @@ async function getuserbyid(id: number) {
 }
 
 onMounted(() => {
-  getuserbyid(route.params.id);
-  getFollowersAndFollowing(route.params.id);
+  getuserbyid(route.params.id as string);
+  getFollowersAndFollowing(route.params.id as string);
   handleGetUser();
   fetchTweets();
 });
@@ -267,69 +267,38 @@ async function fetchTweets() {
             <div>
               <h1 class="mt-1 text-center">Editar perfil</h1>
               <div class="text-subtitle-1 text-medium-emphasis">Nome</div>
-              <v-text-field
-                density="compact"
-                :placeholder="account.name.toUpperCase()"
-                prepend-inner-icon="mdi-account-outline"
-                variant="outlined"
-                v-model="account.name"
-                :error-messages="validationErrors.name"
-              ></v-text-field>
+              <v-text-field density="compact" :placeholder="account.name.toUpperCase()"
+                prepend-inner-icon="mdi-account-outline" variant="outlined" v-model="account.name"
+                :error-messages="validationErrors.name"></v-text-field>
 
               <div class="mt-1 text-subtitle-1 text-medium-emphasis">Sobrenome</div>
-              <v-text-field
-                density="compact"
-                :placeholder="account.surname.toUpperCase()"
-                prepend-inner-icon="mdi-account-outline"
-                variant="outlined"
-                v-model="account.surname"
-                :error-messages="validationErrors.surname"
-              ></v-text-field>
+              <v-text-field density="compact" :placeholder="account.surname.toUpperCase()"
+                prepend-inner-icon="mdi-account-outline" variant="outlined" v-model="account.surname"
+                :error-messages="validationErrors.surname"></v-text-field>
 
               <div class="text-subtitle-1 text-medium-emphasis">Nome de usuário</div>
-              <v-text-field
-                density="compact"
-                :placeholder="account.username"
-                prepend-inner-icon="mdi-account-outline"
-                variant="outlined"
-                v-model="account.username"
-                :error-messages="validationErrors.username"
-              ></v-text-field>
+              <v-text-field density="compact" :placeholder="account.username" prepend-inner-icon="mdi-account-outline"
+                variant="outlined" v-model="account.username"
+                :error-messages="validationErrors.username"></v-text-field>
 
               <div class="mt-1 text-center text-subtitle-1 text-medium-emphasis">
                 Escolha um avatar (opcional):
               </div>
               <div class="d-flex justify-center my-4 ga-2 upload-avatar-container">
-                <v-file-input
-                  class="d-none"
-                  accept="image/png, image/jpeg, image/jpg"
-                  label="Avatar"
-                  @change="bindCustomAvatar"
-                  id="avatar"
-                ></v-file-input>
+                <v-file-input class="d-none" accept="image/png, image/jpeg, image/jpg" label="Avatar"
+                  @change="bindCustomAvatar" id="avatar"></v-file-input>
                 <label class="upload-avatar-label" for="avatar">
                   <v-avatar :image="previewAvatar ?? item.avatar_url" size="75"></v-avatar>
                 </label>
                 <div v-if="validationErrors.avatar.length > 0">
-                  <p
-                    class="error-avatar-message"
-                    v-for="error in validationErrors.avatar"
-                    :key="error"
-                  >
+                  <p class="error-avatar-message" v-for="error in validationErrors.avatar" :key="error">
                     {{ error }}
                   </p>
                 </div>
               </div>
 
-              <v-btn
-                @click="handleEdit"
-                class="mb-2"
-                color="blue"
-                size="large"
-                variant="flat"
-                block
-                :disabled="loadingVisibleModal"
-              >
+              <v-btn @click="handleEdit" class="mb-2" color="blue" size="large" variant="flat" block
+                :disabled="loadingVisibleModal">
                 <span v-if="!loadingVisibleModal"> Editar Perfil </span>
                 <div class="d-flex justify-center" v-if="loadingVisibleModal">
                   <v-progress-circular indeterminate color="white" :size="20" :width="3" />
@@ -346,46 +315,35 @@ async function fetchTweets() {
         <v-row class="border ga-4">
           <v-col class="pa-0 ma-0">
             <v-img class="bg-grey" height="215" aspect-ratio="16/9" cover />
-            <img
-              class="profile-img mx-4 rounded-circle border-md"
-              width="100"
-              height="100"
-              :src="anotherUser.avatar_url ?? default_avatar"
-              alt=""
-            />
+            <img class="profile-img mx-4 rounded-circle border-md" width="100" height="100"
+              :src="anotherUser.avatar_url ?? default_avatar" alt="" />
           </v-col>
           <v-col cols="12" class="d-flex flex-row justify-end ga-2 py-4 py-md-0 px-4">
             <button v-if="item.id === anotherUser.id" @click="editDialog = true">
               <span>Editar</span>
             </button>
-            <v-btn
-              :loading="btnLoading"
-              class="d-flex justify-start align-self-start"
-              height="32"
-              @click="handleFollow"
-            >
+            <v-btn v-if="item.id !== anotherUser.id" :loading="btnLoading" class="d-flex justify-start align-self-start"
+              height="32" @click="handleFollow">
               <span>{{ isFollowing ? 'Seguindo' : 'Seguir' }}</span>
             </v-btn>
           </v-col>
           <v-col class="py-0 px-4">
             <v-list class="py-0">
               <div class="d-flex flex-column">
-                <span class="text-h4 font-weight-bold"
-                  >{{ anotherUser.name }} {{ anotherUser.surname }}</span
-                >
+                <span class="text-h4 font-weight-bold">{{ anotherUser.name }} {{ anotherUser.surname }}</span>
                 <span class="text-h6">@{{ anotherUser.username }}</span>
               </div>
               <div class="d-flex ga-4">
                 <p class="text-h7">
                   <span class="font-weight-bold">{{ anotherUser.followers_count ?? '0' }}</span>
-                  seguidores
+                  Seguindo
                 </p>
                 <p class="text-h7">
                   <span class="font-weight-bold">{{ anotherUser.following_count ?? '0' }}</span>
-                  seguindo
+                  Seguidores
                 </p>
                 <p class="text-h7">
-                  <span class="font-weight-bold">{{ tweets.length }}</span> posts
+                  <span class="font-weight-bold">{{ tweets.length }}</span> Posts
                 </p>
               </div>
             </v-list>
