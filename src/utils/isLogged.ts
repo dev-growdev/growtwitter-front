8c50c1c082067dbc2244d2d3772e6b02cf518c82
Matheus Luna <srmatheus6162@gmail.com>
2024-09-30 17:31:56 -0300

    fix: edited isLogged to better function

--- src/utils/isLogged.ts
+++ src/utils/isLogged.ts
@@ -1,10 +1,12 @@
-import { useRouter } from 'vue-router';
+import { isTokenAuthenticated } from '@/services/authentication';
+import type { Router } from 'vue-router';
 
-export async function isLogged() {
-  const token = sessionStorage.getItem('token') || localStorage.getIem('token');
-  const user = sessionStorage.getItem('userData') || localStorage.getItem('userData');
-  const router = useRouter();
-  if (user && token) return router.push('/'); // verifica se tem user e token
+export async function isLogged(router: Router) {
+  //descobri que o router não pode ser usado dentro de funções no vue3 só pode ser chamada dentro de um "setup" de um componente ou função que seja parte do ciclo de vida de um componente em Vue 3
+  const token = await isTokenAuthenticated();
+  const user = sessionStorage.getItem('userData') ?? localStorage.getItem('userData');
+
+  if (user && token) return router.push('/');
   sessionStorage.removeItem('token');
   return router.push('/login');
 }
