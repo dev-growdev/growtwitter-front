b91dc10b5ce55b97962b55622f2525c11a3b2ddd
Matheus Luna <srmatheus6162@gmail.com>
2024-09-30 17:31:17 -0300

    feat: adding a function to verify the user token is in back end token list

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -1,5 +1,25 @@
+import { client } from './api';
+
 export function isUserAuthenticated() {
   return localStorage.getItem('token') || sessionStorage.getItem('token') ? true : false;
+}
+
+export async function isTokenAuthenticated() {
+  try {
+    const token = getUserToken();
+    const config = {
+      headers: { Authorization: `Bearer ${getUserToken()}` }
+    };
+
+    const response = await client.get(`logout/${token}`, config);
+
+    if (response.status === 200) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export function getUserToken() {
