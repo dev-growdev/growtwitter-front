06a9ec8ff427b99a4847de207a87cf0d4e950433
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-10-10 19:38:17 -0300

    new-card-tweet-design

--- src/components/CardTweet.vue
+++ src/components/CardTweet.vue
@@ -4,6 +4,7 @@ import default_avatar from '@/assets/default-avatar.png';
 import { tempoDesdeCriacao } from '@/utils/PastTime';
 import { postComment, postLike, postRetweet } from '@/services/api';
 import { onMounted, ref } from 'vue';
+import IconComment from './icons/IconComment.vue';
 
 interface TweetTypeProps {
   data: TweetType;
@@ -40,13 +41,10 @@ const toggleDropdown = () => {
   dropdown.value = !dropdown.value;
 };
 const handleRetweet = async (id: number) => {
-
   const response = await postRetweet(id);
   if (response) {
     dropdown.value = false;
   }
-
-
 };
 const handleRetweetWithComment = async (id: number, content: string) => {
   retweetLoading.value = true;
@@ -55,16 +53,12 @@ const handleRetweetWithComment = async (id: number, content: string) => {
     dropdown.value = false;
     retweetModal.value = false;
   }
-
 };
 
 const toggleModalRetweet = () => {
   retweetModal.value = true;
   comment.value = '';
-
 };
-
-
 
 async function handlePostLike(id: number) {
   like();
@@ -77,73 +71,88 @@ async function handlePostLike(id: number) {
 onMounted(() => {
   const user = localStorage.getItem('userData');
   if (user) {
-    liked.value = props.data.likes.some(
-      (like: any) => like.userId == JSON.parse(user).id
-    );
+    liked.value = props.data.likes.some((like: any) => like.userId == JSON.parse(user).id);
   }
-
 });
 </script>
 
 <template>
   <div class="card-principal rounded-0">
-    <v-card-actions class="ga-2">
+    <v-card-actions class="ma-2 ga-2">
       <div class="d-block align-self-start">
-        <RouterLink :to="`/profile/${data.user.id}`"><v-avatar :image="data.user.avatar_url ?? default_avatar"
-            size="50"></v-avatar></RouterLink>
-
-
-
+        <RouterLink :to="`/profile/${data.user.id}`">
+          <v-avatar :image="data.user.avatar_url ?? default_avatar" size="45" />
+        </RouterLink>
       </div>
-      <div class="tweet-body">
-        <div class="tweet-header">
+      <div class="d-flex flex-column justify-space-between w-100">
+        <div class="d-flex align-center justify-space-between w-100">
           <div>
             <RouterLink :to="`/profile/${data.user.id}`">
-              <strong>{{ data.user.name }}</strong> <span>@{{ data.user.username }}</span>
+              <strong class="mouseHover">{{ data.user.name }}</strong> <span>@{{ data.user.username }}</span>
             </RouterLink>
 
             <span> ·</span> <span>{{ tempoDesdeCriacao(data.created_at) }}</span>
+            <p class="tweet-content">{{ data.content }}</p>
           </div>
-          <div style="display: flex; align-items: end; flex-direction: column; position: relative;">
-            <v-btn icon small @click="toggleDropdown"> <v-icon icon="mdi-menu"></v-icon></v-btn>
+
+          <div class="d-flex flex-column align-start position-relative">
+            <v-btn icon small @click="toggleDropdown">
+              <v-icon icon="mdi-menu" />
+            </v-btn>
+
             <div v-if="dropdown" class="dropdown">
               <v-btn small @click="handleRetweet(data.id)"> Retweet</v-btn>
               <v-btn small @click="toggleModalRetweet()"> Retweet com Comentário</v-btn>
             </div>
           </div>
-
         </div>
-        <p class="tweet-content">{{ data.content }}</p>
-        <div class="tweet-actions">
-          <v-btn icon small @click="toogleDiv()">💬{{ data.comments_count }}</v-btn>
-          <v-btn icon small class="btn-like" @click="handlePostLike(data.id)">
-            {{ liked ? '❤️' : '🤍' }}
-            {{ data.likes_count + artificialLike }}
-          </v-btn>
-        </div>
-        <div v-if="showDiv">
-          <hr>
-          <div v-for="comment in props.data.comments" :key="comment.id">
+        <div class="d-flex ga-1">
+          <article class="align-center text-center">
+            <v-btn icon class="btn-comment" @click="toogleDiv()">
+              <IconComment class="icon-comment" />
+              <span>
+                {{ data.comments_count }}
+              </span>
+            </v-btn>
+          </article>
 
-            <div style="display: flex; align-items: center; justify-content: end; width: 100%; margin: 5px 0;">
+          <article class="d-flex">
+            <v-btn icon v-if="!liked" class="btn-like d-flex align-center ga-0" @click="handlePostLike(data.id)">
+              <v-icon icon="mdi-cards-heart-outline" />
+              <span>
+                {{ data.likes_count + artificialLike }}
+              </span>
+            </v-btn>
+
+            <v-btn icon v-if="liked" class="btn-like d-flex align-center ga-0" @click="handlePostLike(data.id)">
+              <v-icon icon="mdi-cards-heart" color="#f91880" />
+              <span>
+                {{ data.likes_count + artificialLike }}
+              </span>
+            </v-btn>
+          </article>
+        </div>
+        <div v-if="showDiv" class="mt-2">
+          <div v-for="comment in props.data.comments" :key="comment.id" class="d-flex flex-column mb-4">
+            <div class="d-flex ga-2 align-center w-100 mx-2">
               <RouterLink :to="`/profile/${comment.user.id}`">
-                <v-avatar :image="comment.user.avatar_url ?? default_avatar" size="25"></v-avatar>
+                <v-avatar :image="comment.user.avatar_url ?? default_avatar" size="45"></v-avatar>
               </RouterLink>
               <div>
-                <RouterLink :to="`/profile/${comment.user.id}`">
-                  <strong>{{ comment.user.name }}</strong> <span>@{{ comment.user.username }}</span>
-                </RouterLink>
+                <div>
+                  <RouterLink :to="`/profile/${comment.user.id}`">
+                    <span class="mouseHover font-weight-bold">{{ comment.user.name }}</span> <span style="color: #657786">@{{ comment.user.username }}</span>
+                  </RouterLink>
 
-                <span> ·</span> <span>{{ tempoDesdeCriacao(comment.created_at) }}</span>
+                  <span style="color: #657786"> ·</span> <span style="color: #657786">{{ tempoDesdeCriacao(comment.created_at) }}</span>
+                </div>
+                <div class="d-flex align-center w-100 mt-0 text-h7">
+                  {{ comment.content }}
+                </div>
               </div>
             </div>
-            <div style="font-size: 12px; width: 100%; text-align: end; font-weight: bold;">{{
-              comment.content }}</div>
-
-
           </div>
           <form @submit.prevent="handleSubmit(data.id)">
-
             <div class="text-box">
               <div class="box-container">
                 <textarea placeholder="Comentar" v-model="commentInput"></textarea>
@@ -151,20 +160,20 @@ onMounted(() => {
                 <div class="formatting">
                   <button type="submit" class="send" title="Send">
                     <svg fill="none" viewBox="0 0 24 24" height="18" width="18" xmlns="http://www.w3.org/2000/svg">
-                      <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#ffffff"
-                        d="M12 5L12 20"></path>
-                      <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#ffffff"
-                        d="M7 9L11.2929 4.70711C11.6262 4.37377 11.7929 4.20711 12 4.20711C12.2071 4.20711 12.3738 4.37377 12.7071 4.70711L17 9">
-                      </path>
+                      <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#ffffff" d="M12 5L12 20"></path>
+                      <path
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                        stroke-width="2.5"
+                        stroke="#ffffff"
+                        d="M7 9L11.2929 4.70711C11.6262 4.37377 11.7929 4.20711 12 4.20711C12.2071 4.20711 12.3738 4.37377 12.7071 4.70711L17 9"
+                      ></path>
                     </svg>
                   </button>
                 </div>
-
               </div>
             </div>
-
           </form>
-
         </div>
       </div>
     </v-card-actions>
@@ -180,9 +189,9 @@ onMounted(() => {
         <v-card class="mt-4" outlined>
           <v-card-text>
             <div class="d-flex align-center">
-              <v-avatar :image="data.user.avatar_url ?? default_avatar" size="50"></v-avatar>
+              <v-avatar :image="data.user.avatar_url ?? default_avatar" size="45"></v-avatar>
               <div class="ml-3">
-                <div class="font-weight-bold">{{ data.user.name }}</div>
+                <div class="mouseHover font-weight-bold">{{ data.user.name }}</div>
                 <div class="text--secondary">@{{ data.user.username }}</div>
               </div>
             </div>
@@ -193,12 +202,10 @@ onMounted(() => {
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="blue darken-1" @click="retweetModal = false">Cancelar</v-btn>
-        <v-btn :loading="retweetLoading" color="blue darken-1"
-          @click="handleRetweetWithComment(data.id, comment)">Retweet</v-btn>
+        <v-btn :loading="retweetLoading" color="blue darken-1" @click="handleRetweetWithComment(data.id, comment)">Retweet</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
-
 </template>
 
 <style scoped>
@@ -207,6 +214,45 @@ onMounted(() => {
   transition: background-color 0.3s ease;
 }
 
+.icon-comment {
+  fill: #808080 !important;
+}
+
+.btn-comment:hover {
+  color: #2c8cd4 !important;
+  fill: #2c8cd4;
+}
+
+.btn-comment:hover .icon-comment {
+  color: #2c8cd4 !important;
+  fill: #2c8cd4 !important;
+}
+
+.btn-like {
+  text-transform: none !important;
+  user-select: none;
+  cursor: pointer;
+}
+
+.btn-like:hover {
+  color: #f91880 !important;
+}
+
+.btn-like:hover .mdi-cards-heart-outline {
+  color: #f91880 !important;
+}
+
+.mdi-cards-heart-outline {
+  color: #808080;
+}
+
+.mouseHover {
+  transition: all 0.2s ease;
+}
+
+.mouseHover:hover {
+  text-decoration: underline;
+}
 
 .dropdown {
   display: flex;
@@ -222,21 +268,11 @@ onMounted(() => {
   z-index: 10;
 }
 
-
-
-.tweet-body {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 100%;
-}
-
 .tweet-header {
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 100%;
-
 }
 
 .tweet-header strong {
@@ -253,19 +289,9 @@ onMounted(() => {
   word-break: break-word;
 }
 
-.v-btn {
-  text-transform: none !important;
-}
-
-.btn-like:hover {
-  filter: drop-shadow(1px 1px 1px red);
-  color: rgb(135, 0, 0);
-}
-
 .text-box {
   width: 100%;
   height: fit-content;
-
   padding: 8px;
 }
 
@@ -286,8 +312,6 @@ onMounted(() => {
   outline: none;
   caret-color: #0a84ff;
 }
-
-
 
 .text-box .formatting button {
   width: 30px;
