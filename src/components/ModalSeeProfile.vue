ea1c214d73dba5c0b1655a2f06e4dbd2c16e4510
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-10-14 18:03:25 -0300

    add-new-component

--- src/components/ModalSeeProfile.vue
+++ src/components/ModalSeeProfile.vue
@@ -1,39 +1,90 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import type { UserType } from '../types/UserType';
+import default_avatar from '@/assets/default-avatar.png';
+import { postFollow } from '@/services/api';
+import { RouterLink } from 'vue-router';
 
-const props = defineProps<UserType>()
+interface ModalTypeProps {
+  data: UserType;
+}
 
-const seeModal = ref<boolean>(false)
+const props = defineProps<ModalTypeProps>();
+const btnLoading = ref<boolean>(false);
+const isFollowing = ref<boolean>(false);
 
+const myUserData = JSON.parse(localStorage.getItem('userData') || '""');
+
+let itsMe = myUserData.id != props.data.id;
+
+async function handleFollow() {
+  try {
+    btnLoading.value = true;
+
+    if (itsMe) {
+      const response = await postFollow(String(props.data.id), myUserData.id);
+
+      if (response.data.msg === 'Seguido com sucesso') {
+        isFollowing.value = true;
+      }
+      if (response.data.msg === 'Deixou de seguir') {
+        isFollowing.value = false;
+      }
+
+      if (response.data.msg === 'Falha') {
+        isFollowing.value = true;
+      }
+      btnLoading.value = false;
+    }
+  } catch {
+    console.log({ success: false, msg: 'Não foi possível seguir o usuário' });
+  }
+}
 </script>
 
 <template>
-    <v-dialog v-model="seeModal" max-width="500px">
-        <v-card>
-          <v-card-title>
-            <span class="text-h5">Retweetar com comentário</span>
-          </v-card-title>
-          <v-card-text>
-            <v-card class="mt-4" outlined>
-              <v-card-text>
-                <div class="d-flex align-center">
-                  <v-avatar :image="props.avatar_url ?? default_avatar" size="50"></v-avatar>
-                  <div class="ml-3">
-                    <div class="font-weight-bold">{{ props.name }}</div>
-                    <div class="text--secondary">@{{ props.username }}</div>
-                  </div>
-                </div>
-                <div class="mt-3 ml-13">{{ props. }}</div>
-              </v-card-text>
-            </v-card>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" @click="retweetModal = false">Cancelar</v-btn>
-            <v-btn :loading="retweetLoading" color="blue darken-1"
-              @click="handleRetweetWithComment(data.id, comment)">Retweet</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+  <div class="flex flex-col align-end position-relative">
+    <section class="d-flex flex-row justify-space-between position-absolute bg-grey-lighten-4 ga-2 pa-4 rounded-lg dropdown" style="min-width: 220px; max-width: 300px">
+      <article class="d-block align-start">
+        <RouterLink :to="`/profile/${data.id}`"><v-avatar :image="data.avatar_url ?? default_avatar" size="75"></v-avatar></RouterLink>
+
+        <div>
+          <RouterLink :to="`/profile/${props.data.id}`" class="text-blue-darken-1 font-weight-bold text-h6" v-if="itsMe">
+            {{ props.data.name }}
+          </RouterLink>
+          <RouterLink :to="`/profile/${props.data.id}`" class="text-blue-darken-1 font-weight-bold text-h6" v-if="!itsMe">
+            {{ props.data.name }}
+            <span class="text-grey-darken-1 text-subtitle-2 font-weight-regular">(Você)</span>
+          </RouterLink>
+        </div>
+        <span class="text-grey-darken-1 font-weight-regular">@{{ props.data.username }}</span>
+      </article>
+      <article class="d-flex justify-center align-top">
+        <v-btn id="followingSpan" v-if="data.id !== myUserData.id" :loading="btnLoading" class="bg-black rounded-xl" height="32" @click="handleFollow()">
+          {{ isFollowing ? 'Seguindo' : 'Seguir' }}
+        </v-btn>
+      </article>
+    </section>
+  </div>
 </template>
+
+<style scoped>
+section {
+  box-shadow:
+    rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+}
+
+.dropdown {
+  top: 0;
+  left: -140%;
+  max-width: 300px;
+  min-width: 225px;
+  z-index: 999;
+}
+
+#followingSpan {
+  font-size: small;
+  font-weight: 600;
+}
+</style>
